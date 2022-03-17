8bd1e04f3083f4dd2a831e16044b8383470b5185
Muhammed Halid GAMZE <81425579+Muhammed-Halid-Gamze@users.noreply.github.com>
2022-03-17 12:38:24 +0300

    Add files via upload

---
+++ to-do-app project/to-do-app.js
@@ -0,0 +1,21 @@
+// variables
+
+const addList = document.getElementById("input");
+console.log(addList);
+
+const addButton = document.getElementById("button");
+addButton.addEventListener("click", () => {
+    let a = addList.value;
+
+    console.log(a);
+});
+
+
+
+
+
+
+// functions
+
+
+//
